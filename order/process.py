4498d70754349afc3be3dcc78751ce60789367ef
MsrdMai <sirilada.mai@gmail.com>
2022-03-17 15:39:38 +0700

    null

--- order/process.py
+++ order/process.py
@@ -6,10 +6,16 @@ from xhtml2pdf import pisa
 import pythainlp
 # defining the function to convert an HTML file to a PDF file
 def html_to_pdf(template_src, context_dict={}):
-     template = get_template(template_src)
-     html  = template.render(context_dict)
-     result = BytesIO()
-     pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
-     if not pdf.err:
-         return HttpResponse(result.getvalue(), content_type='application/pdf')
-     return None
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    #  pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+    pdf = pisa.pisaDocument(
+                src=BytesIO(html.encode('UTF-8')),
+                dest=result,
+                encoding='UTF-8'
+            )
+
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
