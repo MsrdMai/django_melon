4f3960fc577e2e16094906d4c5417ad301c4f074
MsrdMai <sirilada.mai@gmail.com>
2022-04-01 14:40:32 +0700

    add Admin.py ab

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
-
+from order.models import Order, State, OrderCarving, Payment, Disease, Bug, Record, CovertImage, CancelOrder, Portage, Review, Message
 # Register your models here.
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'state_name']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_id_id', 'date_time', 'total_price', 'amount','shipping_cost','State_id_id','address','tel','User_id']
+
+class OrderCarvingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id_id', 'image', 'carving_description']
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id_id', 'payment_image', 'bank_name']
+
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'disease_name','disease_description']
+
+class BugAdmin(admin.ModelAdmin):
+    list_display = ['id', 'bug_name','bug_description']
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id_id', 'recode_date', 'recode_description','recode_image','recode_weight','sweetness','quality_id_id']
+
+class CovertImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'covert_image', 'recode_id_id']
+
+class CancelOrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id_id', 'cancel_description', 'refund_contact','disease_id_id','bug_id_id']
+
+class PortageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id_id', 'portage_name', 'portage_number','date_send']
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id_id', 'review_description', 'review_image','review_rating','date_send']
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id_id', 'content', 'date_added','User_id']
+    
+admin.site.register(State, StateAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderCarving, OrderCarvingAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Disease, DiseaseAdmin)
+admin.site.register(Bug, BugAdmin)
+admin.site.register(Record, RecordAdmin)
+admin.site.register(CovertImage, CovertImageAdmin)
+admin.site.register(CancelOrder, CancelOrderAdmin)
+admin.site.register(Portage, PortageAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Message, MessageAdmin)
--- store/admin.py
+++ store/admin.py
@@ -4,7 +4,21 @@ from store.models import Store, TypeeOrder, Quality, Product
 # Register your models here.
 
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_id', 'store_name', 'store_logo', 'store_img',]
+    list_display = ['id', 'user_id', 'store_name', 'store_logo', 'store_img','store_address','store_description','store_phone','bank_account','name_bank','bank_id']
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'store_id_id', 'product_name', 'melon_color', 'product_description','product_price','product_weight','product_amount','range','product_image','date_create',
+    'date_expire','date_harvest','shipping_cost','select_carving','cost_carving','type_id_id','quality_id_id']
+
+class TypeeOrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class QualityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'quality_name', 'quality_description']
 
 admin.site.register(Store, StoreAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Quality, QualityAdmin)
+admin.site.register(TypeeOrder, TypeeOrderAdmin)
